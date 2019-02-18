56247b0f7d1dd36a8dff71f14cf39ab3bcebc02d
root <root@127.0.1.1>
2019-02-18 16:34:32 +0000

    Fixed erros in relay

--- modules/relay/relay.py
+++ modules/relay/relay.py
@@ -12,15 +12,15 @@ CONFIG = {
     #you can insert anything you want in attrs attribute
     #it will be given to on_read or on_write function
     "DEVICES" : [
-        {'name' : 'relay1', 'size' : 1, 'attrs' : dict(gpio = '/sys/class/gpio/A23')},
-        {'name' : 'relay1', 'size' : 1, 'attrs' : dict(gpio = '/sys/class/gpio/A24')},
+        {'name' : 'relay1', 'size' : 1, 'attrs' : { 'gpio' : '/sys/class/gpio/pioA23/value'}},
+        {'name' : 'relay2', 'size' : 1, 'attrs' : {'gpio' : '/sys/class/gpio/pioA24/value'}}
     ]
 }
 
 #This function is for starting the logic
 def on_read(device, size, offset):
     attr = device[1]
-    with open(attr.gpio, 'rb') as f:
+    with open(attr['gpio'], 'rb') as f:
         data = f.read()
         return data
 
@@ -28,9 +28,8 @@ def on_read(device, size, offset):
 
 def on_write(device, value):
     attr = device[1]
-    with open(attr.gpio, 'wb') as f:
+    with open(attr['gpio'], 'wb') as f:
         f.write(value)
-        return data
 
     return len(value)
 
