7441f05c9c2d4ccfbabda5bdaa82ff08061b5e17
Felix Fröhling <felix.froehling1@gmail.com>
2019-02-18 11:22:13 +0100

    Updated modules

--- modules/relay/relay.py
+++ modules/relay/relay.py
@@ -14,21 +14,8 @@ CONFIG = {
     "DEVICES" : [
         {'name' : 'relay1', 'size' : 16, 'attrs' : dict()},
         {'name' : 'relay2', 'size' : 16, 'attrs' : dict()}
-#        {'name' : 'relay3', 'typ' : 'c'},
-#        {'name' : 'relay4', 'typ' : 'c'},
-#        {'name' : 'relay5', 'typ' : 'c'}
     ]
 }
-
-#This function is called before initalization from the runtime environment
-def pre_initialize():
-    pass
-    print("Module will be initialized")
-
-#This function will be called immediatly after the initalization. It gets the devices parameter
-def post_initialize(devices):
-    print("Module has been initialized")
-    devices[0].add_read_watch()
 
 #This function is for starting the logic
 def on_read(device, size, offset):
@@ -44,15 +31,6 @@ def on_write(device, value):
     print(value)
 
     return len(value)
-
-#This function will be called right before runtime stops the module. Clean up your resources here
-def pre_stop():
-    print("Module will be stopped")
-    pass
-
-#After stopping you can do sth, e.g. logging
-def post_stop():
-    print("Module has been stopped")
 
 ###END SECTION COMPONENT_MODEL
 
