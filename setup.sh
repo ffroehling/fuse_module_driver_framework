3b8a9af83b8aaf4140a3684bb9b07b5f26a0e734
Felix Fröhling <froehling@lab-quade.de>
2019-02-26 18:15:02 +0100

    Bugfix :wq

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #set environment variable
-echo 'FUSE_LIBRARY_PATH=/usr/lib/libfuse.so' > ~/.profile
+echo 'export FUSE_LIBRARY_PATH=/usr/lib/libfuse.so' > ~/.profile
 source ~/.profile
 
 pip install -r requirements.txt
