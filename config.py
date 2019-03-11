542cdddd205f064f8de1d1de77720ece9aefe625
Felix Fröhling <froehling@lab-quade.de>
2019-03-11 16:31:13 +0100

    Changed development values to production values and removed files

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BASEPATH = '/dev/test'
+BASEPATH = '/dev/abstraction'
 fifo_in_file = '/tmp/abstractionfifo_in'
 fifo_out_file = '/tmp/abstractionfifo_out'
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FUSE_LIBRARY_PATH=/usr/lib/libfuse.so
-cd kernelabstraction
-python3 abstraction.py &  > /tmp/log
