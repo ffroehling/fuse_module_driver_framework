44f74ecf7de573c5a87e05feef6bdf466856f770
Felix Fröhling <froehling@lab-quade.de>
2019-03-11 15:40:42 +0100

    Added modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,8 @@
-#from . import relay
-#from . import dac
+from . import relay
+from . import dac
 from . import onewire
 from . import dht
-#from . import oled
+from . import oled
 
 #These are the modules which are started automaticly upon starting the abstraction
 start = [
@@ -12,9 +12,9 @@ start = [
 
 #Each module needs to be registered here
 all_modules =  [
-#    relay,
-#    dac,
+    relay,
+    dac,
     onewire,
     dht,
-#    oled
+    oled
 ]
